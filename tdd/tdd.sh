803590f1dafa0b764209eed9c12f5d1813128d2e
hamzas <skander.hamza@st.com>
2017-08-10 15:58:49 +0100

    tdd: improve gold card parsing

--- tdd/tdd.sh
+++ tdd/tdd.sh
@@ -5,9 +5,18 @@ DEFAULT_ENCOURS="encours_default.csv"
 DEFAULT_RESULT="default.qif"
 DEFAULT_OUTPUT="listMvmts.qif"
 
+GOLD_CARD_PARSE="improvedGoldCardParse.qif"
+
 ../amenCsvToQif.bash $DEFAULT_LIST $DEFAULT_ENCOURS
 if cmp -s "$DEFAULT_OUTPUT" "$DEFAULT_RESULT"; then
    echo "DEFAULT TEST      PASS"
 else
    echo "DEFAULT TEST      FAIL"
+fi
+
+if cmp -s "$DEFAULT_OUTPUT" "$GOLD_CARD_PARSE"; then
+   echo "GOLD CARD TEST      PASS"
+else
+   echo "GOLD CARD TEST      FAIL"
+   diff $DEFAULT_OUTPUT $GOLD_CARD_PARSE
 fi
